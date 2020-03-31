3c16505c2bed67138aa479381d3f4ee7132e911a
Harsh Gupta <harshgupta951114@gmail.com>
2020-03-31 15:46:33 +0530

    resolve refresh and reconnect message issue

--- server/index.js
+++ server/index.js
@@ -39,8 +39,12 @@ io.on('connection',(socket)=>{
         callback();
     });
 
-    socket.on('disconnet', ()=>{
-        console.log('User left the server');
+    socket.on('disconnect', ()=>{
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left`});
+        }
     });
 });
 
