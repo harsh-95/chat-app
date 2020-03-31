02668f3f661dd2ba0e6edf4485ad1d28d5c5299d
Harsh Gupta <harshgupta951114@gmail.com>
2020-03-31 12:16:23 +0530

    message and sendMessage socket server side

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const routes = require('./router');
+const {addUser, removeUser, getUser, getUserInRoom} = require('./users');
 
 const app = express();
 
@@ -17,7 +18,25 @@ io.on('connection',(socket)=>{
     console.log('socket connected');
 
     socket.on('join', ({name, room}, callback)=>{
-        console.log(name, room);
+        const {user, error} = addUser({id: socket.id, name, room});
+
+        if(error){
+            return callback(error);
+        }
+
+        socket.emit('message', {user: 'admin', text: `${user.name} welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message',{user: 'admin', text: `${user.name} has joined`});
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', ({message, callback})=>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
     });
 
     socket.on('disconnet', ()=>{
